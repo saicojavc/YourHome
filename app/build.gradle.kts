618148016f8e0ce33aea1c727878ea8d84ec810e
Jorge Adrian Valdes Camacho <jorgito.avc03@gmail.com>
2025-04-29 23:17:38 -0400

    configuration gradle main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,22 +2,32 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+//    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
     namespace = "com.saico.yourhome"
-    compileSdk = 35
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "com.saico.yourhome"
-        minSdk = 24
-        targetSdk = 35
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    android.applicationVariants.configureEach {
+        //Example (com.saico.airlineticket-v0.0.1-release.apk)
+        val name = buildType.name // buildType [debug,release]
 
+        outputs
+            .map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach {
+                it.outputFileName = "${applicationId}-v${versionName}-${name}.apk"
+            }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -26,20 +36,55 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildFeatures {
         compose = true
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
+
+//    implementation(project(":core:ui"))
+//    implementation(project(":feature:home"))
+//    implementation(project(":feature:login"))
+//    implementation(project(":feature:seat"))
+//    implementation(project(":feature:ticket_detail"))
+
+    implementation(libs.androidx.lifecycle.runtime.compose.android)
+    implementation(libs.androidx.navigation.common.ktx)
+//    implementation(libs.firebase.database)
+    coreLibraryDesugaring(libs.com.android.tools.desugar)
+
+    //SplashScreen
+    implementation(libs.androidx.core.core.splashscreen)
+
+    //hilt
+    implementation(libs.com.google.dagger.hilt.android)
+//    ksp(libs.com.google.dagger.hilt.compiler)
+
+    implementation(libs.androidx.constraintlayout.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -56,4 +101,15 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.io.coil.kt)
+    implementation(libs.accompanist.pager.indicators)
+    implementation(libs.accompanist.pager)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.runtime.livedata)
+    implementation(libs.glide)
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation(libs.androidx.foundation)
+    implementation(libs.accompanist.systemuicontroller)
 }
