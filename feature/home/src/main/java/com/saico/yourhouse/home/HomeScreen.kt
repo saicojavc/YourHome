a7e6a156b05aa4716781a344e71935a994a98426
Jorge Adrian Valdes Camacho <jorgito.avc03@gmail.com>
2025-05-14 23:32:51 -0400

    navigation home and bottomBar

--- feature/home/src/main/java/com/saico/yourhouse/home/HomeScreen.kt
+++ feature/home/src/main/java/com/saico/yourhouse/home/HomeScreen.kt
@@ -1,9 +1,5 @@
-@file:Suppress("UNUSED_EXPRESSION")
-
 package com.saico.yourhouse.home
 
-
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
@@ -11,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.Home
@@ -25,15 +21,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.google.firebase.auth.FirebaseAuth
 import com.saico.yourhome.ui.component.AppNavigationBar
 import com.saico.yourhome.ui.component.AppNavigationBarItem
-import com.saico.yourhome.ui.navigation.route.login.LoginRoute
 import com.saico.yourhouse.home.model.BottomAppBarItem
-import com.saico.yourhouse.home.model.BottomAppBarItem.HOME
 import com.saico.yourhouse.home.screens.FavoriteScreen
 import com.saico.yourhouse.home.screens.MainHomeScreen
 import com.saico.yourhouse.home.screens.SettingScreen
