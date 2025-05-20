322745849fbe37744f15a6c48c94b68ad88a4dc6
Jorge Adrian Valdes Camacho <jorgito.avc03@gmail.com>
2025-05-20 00:49:18 -0400

    house cart

--- feature/home/src/main/java/com/saico/yourhouse/home/screens/MainHomeScreen.kt
+++ feature/home/src/main/java/com/saico/yourhouse/home/screens/MainHomeScreen.kt
@@ -7,15 +7,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Bathroom
+import androidx.compose.material.icons.rounded.Bed
 import androidx.compose.material.icons.rounded.ExitToApp
+import androidx.compose.material.icons.rounded.LocationOn
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -23,8 +31,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.google.firebase.auth.FirebaseAuth
 import  com.saico.yourhome.ui.R
@@ -43,11 +55,18 @@ fun MainHomeScreen(
         R.drawable.cat_4,
         R.drawable.cat_5
     )
+    val houses = listOf(
+        R.drawable.house_1,
+        R.drawable.house_2,
+        R.drawable.house_3,
+        R.drawable.house_4,
+    )
 
     Content(
         navHostController = navHostController,
-        auth =auth,
-        typeHouseIcons =typeHouseIcons
+        auth = auth,
+        typeHouseIcons = typeHouseIcons,
+        houses = houses
     )
 
 }
@@ -56,7 +75,8 @@ fun MainHomeScreen(
 fun Content(
     typeHouseIcons: List<Int>,
     auth: FirebaseAuth,
-    navHostController: NavHostController
+    navHostController: NavHostController,
+    houses: List<Int>
 ) {
 
     Scaffold(
@@ -92,15 +112,133 @@ fun Content(
                     .padding(8.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                items(typeHouseIcons){ typeHouseIcon ->
+                items(typeHouseIcons) { typeHouseIcon ->
                     TypeHouseList(
                         typeHouseIcon = typeHouseIcon
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.padding(vertical = 8.dp))
+
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+            ) {
+                items(houses){ house ->
+                    HouseCart(
+                        house = house
                     )
                 }
             }
         }
     }
 
+}
+
+@Composable
+fun HouseCart(house: Int) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        onClick = {},
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ){
+            Image(
+                modifier = Modifier
+                    .width(310.dp)
+                    .height(120.dp),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(id = house),
+                contentDescription = null
+            )
+            Row(
+                modifier = Modifier
+                    .width(310.dp)
+//                    .fillMaxWidth()
+                    .padding(12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "House Type",
+                    fontWeight = FontWeight.Bold,
+                )
+
+                Row(
+                    horizontalArrangement = Arrangement.End
+                ){
+                    Icon(
+                        imageVector = Icons.Rounded.Star,
+                        contentDescription = null,
+                        tint = Color.Yellow
+                    )
+                    Text(
+                        text = "4.3",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    modifier = Modifier.padding(start = 8.dp),
+                    imageVector = Icons.Rounded.LocationOn,
+                    contentDescription = null
+                )
+                Text(
+                    text = "Miami",
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .width(310.dp)
+//                    .fillMaxWidth()
+                    .padding(12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Row(
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Bed,
+                        contentDescription = null
+                    )
+                    Text(
+                        text = "2",
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    Spacer(modifier = Modifier.padding(horizontal = 4.dp))
+                    Icon(
+                        imageVector = Icons.Rounded.Bathroom,
+                        contentDescription = null
+                    )
+                    Text(
+                        text = "2",
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.End
+                ){
+
+                    Text(
+                        text = "$500.000",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -118,8 +256,7 @@ fun TypeHouseList(
                 .padding(12.dp),
 
 
-
-            ){
+            ) {
             Image(
                 modifier = Modifier
                     .size(35.dp),
